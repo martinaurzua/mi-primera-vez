ac022c0d6640a4d1af35857520ffea6c7b236754
Martina Urzúa <martina.urzua@ug.uchile.cl>
2025-08-11 17:42:40 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,14 +2,14 @@ var d;
 
 function setup() {
 createCanvas(windowWidth, windowHeight).position(0,0).style('z-index','-1');
-  background(120, 39, 143);
+  background(120,39,143);
 }
 
 function draw() {
-  d = random(10,60);
+  d = random(10,60)
   colorMode(HSB,360,100,100);
   fill(random(0,360),100,100);
-  ellipse(mouseX,mouseY,d,d);
+  ellipse(mouseX,mouseY,d,d)
 }
 
 function keyPressed() {
